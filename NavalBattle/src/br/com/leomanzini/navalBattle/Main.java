0ee2755b648dc803f39c6e305448199aba846189
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-01-07 18:56:32 -0300

    Adding some features, with the time i'll be modified, but for now it's ok!

--- NavalBattle/src/br/com/leomanzini/navalBattle/Main.java
+++ NavalBattle/src/br/com/leomanzini/navalBattle/Main.java
@@ -1,13 +1,73 @@
 package br.com.leomanzini.navalBattle;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main 
-{
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		sc.close();
+public class Main {
+	
+	//Declaring some global variables
+	static int coordinateX, coordinateY, numberOfShips, maxNumberOfShips;
+	static int boardPlayer1[][], boardPlayer2[][];
+	
+	public static void getBoardSize () {
+		Scanner sc = null;
+		for(;;) {
+			boolean ex = false;
+			try {
+				sc = new Scanner(System.in);
+				System.out.println("Enter with the board width: ");
+				coordinateX = sc.nextInt();
+				
+				System.out.println("Enter with the board height: ");
+				coordinateY = sc.nextInt();
+				
+				ex = true;
+			} catch(InputMismatchException err) {
+				System.out.println("Enter with a integer number!\n");
+			}
+			if(ex) {
+				break;
+			}
+		}
+	}
+	
+	public static void calculateMaxNumberOfShips () {
+		maxNumberOfShips = (coordinateX * coordinateY) / 3;
+	}
+	
+	public static void getNumberOfShips () {
+		Scanner sc = null;
+		for(;;) {
+			boolean ex = false;
+			try {
+				sc = new Scanner(System.in);
+				System.out.println("Max number of ships for the currents boards: " + maxNumberOfShips);
+				System.out.println("Enter with the number of ships: ");
+				numberOfShips = sc.nextInt();
+				
+				if(numberOfShips <= 0 || numberOfShips > maxNumberOfShips) {
+					System.out.println("Enter with a valid number of ships!\n");
+				} else {
+					ex = true;
+					break;
+				}
+			} catch(InputMismatchException err) {
+				System.out.println("Enter with a integer number!\n");
+			}
+			if(ex) {
+				break;
+			}
+		}
+	}
+	
+	public static void setPlayerBoard () {
+		boardPlayer1 = new int [coordinateX][coordinateY];
+		boardPlayer2 = new int [coordinateX][coordinateY];
+	}
+	
+	public static void main(String args[]) {
+		getBoardSize();
+		calculateMaxNumberOfShips();
+		getNumberOfShips();
 	}
 }
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A little sea battle game with what I know about Java.
 It consists of a field with X by Y blocks; 
 The player can choose between play with another player or play with the
 computer;
-Any player can have xpto ships;
+Any player can have xpto ships within 1/3 size limit of the board;
 The ships are inserted randomly;
 The game should show each board separately every shift;
 Each shift, the player will choose where It wants to "Shoot";
