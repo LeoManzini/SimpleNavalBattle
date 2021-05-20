0efc65382808b3d0202f8ff891e756a64d857244
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-05-20 20:23:24 -0300

    Adding a menu and a validation to the game choice

--- NavalBattleRefactor/src/main/java/br/com/leomanzini/naval/MainGame.java
+++ NavalBattleRefactor/src/main/java/br/com/leomanzini/naval/MainGame.java
@@ -1,7 +1,28 @@
 package br.com.leomanzini.naval;
 
+import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import br.com.leomanzini.naval.entities.Board;
+
 public class MainGame {
 	public static void main(String args[]) {
 		
+		final Logger LOG = LogManager.getLogger(MainGame.class);
+		Scanner sc = new Scanner(System.in);
+		
+		LOG.info("Choose a game board size: ");
+		LOG.info("1 - Small game board;");
+		LOG.info("2 - Medium game board;");
+		LOG.info("3 - Big game board;");
+		LOG.info("4 - Bigger game board;");
+		
+		int option = sc.nextInt();
+
+		Board board = new Board(option);
+		
+		sc.close();
 	}
 }
--- NavalBattleRefactor/src/main/java/br/com/leomanzini/naval/entities/Board.java
+++ NavalBattleRefactor/src/main/java/br/com/leomanzini/naval/entities/Board.java
@@ -1,105 +1,78 @@
 package br.com.leomanzini.naval.entities;
 
 import java.util.InputMismatchException;
-import java.util.Random;
 import java.util.Scanner;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import br.com.leomanzini.naval.utils.ShipPositions;
-
 public class Board {
-	
+
 	private static final Logger LOG = LogManager.getLogger(Board.class);
-	private int boardSize;
 	private int coordinateX;
 	private int coordinateY;
-	private int playerOneBoard[][];
-	private int playerTwoBoard[][];
-	private ShipPositions positions;
 	private Scanner sc = new Scanner(System.in);
-	
-	public Board(int coordinateX, int coordinateY) {
-		validateCoordinates(coordinateX, coordinateY);
+
+	public Board() {
+	}
+
+	public Board(int option) {
+		validateOption(option);
+		close();
 	}
 	
-	public void validateCoordinates(int coordinateX, int coordinateY) {
-		while(true) {
-			boolean ex = false;
-			try {
-				if (coordinateX > 26 || 0 > coordinateX) {
-					while (coordinateX > 26 || 0 > coordinateX) {
-						LOG.info("Max number of lines is 26, enter with a valid input!");
-						coordinateX = sc.nextInt();
-					}
+	public void validateOption(int option) {
+		try {
+			loop : while (true) {
+				switch(option) {
+					case 1:
+						setCoordinateX(8);
+						setCoordinateY(8);
+						break loop;
+					case 2:
+						setCoordinateX(13);
+						setCoordinateY(13);
+						break loop;
+					case 3:
+						setCoordinateX(20);
+						setCoordinateY(20);
+						break loop;
+					case 4:
+						setCoordinateX(26);
+						setCoordinateY(26);
+						break loop;
+					default:
+						LOG.info("Choose a valid game option: ");
+						option = sc.nextInt();
 				}
-				LOG.info("Enter with the board columns: ");
-				coordinateY = sc.nextInt();
-				ex = true;
-			} catch(InputMismatchException e) {
-				LOG.error("Enter with a integer number!\n", e);
 			}
-			if(ex) {
-				this.coordinateX = coordinateX;
-				this.coordinateY = coordinateY;
-				break;
-			}
-		}  
-	}
-	
-	public void setPlayerBoard () {
-		this.playerOneBoard = createVoidBoard();
-		this.playerTwoBoard = createVoidBoard();
+		} catch(InputMismatchException e) {
+			LOG.error("Enter with a integer number.", e);
+			System.exit(-1);
+		}
 	}
 	
 	public int[][] createVoidBoard () {
 		return new int [this.coordinateX][this.coordinateY];
 	}
 	
-	public int[][] insertShipsAtNewBoard (int numberOfShips) {
-		int[][] newBoard = createVoidBoard();
-		int remainingNumberOfShips = numberOfShips;
-		Random randNumber = new Random();
-		int x = 0, y = 0;
-		do {
-			x = 0;
-			y = 0;
-			for (int[] lines : newBoard) {
-				for (int columns : lines) {
-					if (randNumber.nextInt(100) <= 10) {
-						if (columns == ShipPositions.NOTHING.getIntValue()) {
-							newBoard[x][y] = ShipPositions.SHIP.getIntValue();
-							remainingNumberOfShips--;
-							break;
-						}
-						if (remainingNumberOfShips <= 0) {
-							break;
-						}
-					}
-					y++;
-				}
-				y = 0;
-				x++;
-				if (remainingNumberOfShips <= 0) {
-					break;
-				}
-			}
-		} while(remainingNumberOfShips > 0);
-		return newBoard;
+	public void close() {
+		sc.close();
 	}
-	
-	public void insertShipsAtPlayersBoard (int numberOfShips) {
-		this.playerOneBoard = insertShipsAtNewBoard(numberOfShips);
-		this.playerTwoBoard = insertShipsAtNewBoard(numberOfShips);
+
+	public int getCoordinateX() {
+		return coordinateX;
 	}
-	
-	public void printNumbering () {
-		int columnNumber = 1;
-		String boardNumber = "   ";
-		for(int i = 0; i < this.coordinateY; i++) {
-			boardNumber += (columnNumber++) + " ";
-		}
-		System.out.println(boardNumber);
+
+	public void setCoordinateX(int coordinateX) {
+		this.coordinateX = coordinateX;
+	}
+
+	public int getCoordinateY() {
+		return coordinateY;
+	}
+
+	public void setCoordinateY(int coordinateY) {
+		this.coordinateY = coordinateY;
 	}
 }
